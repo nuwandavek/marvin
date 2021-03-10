777987dfeecc88b45e62aed4e0b59f353c40cf29
Daniel Estevan McAndrew <daniel.e.mcandrew@gmail.com>
2021-03-09 18:31:49 -0800

    Minor refactor and adding comments.

--- app/server.py
+++ app/server.py
@@ -1,15 +1,18 @@
+# import flask related modules
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 
+# basic imports
 import pickle
 import json
 import sys
 
+# Pytorch imports
 import torch
 from torchtext.data.utils import get_tokenizer
 
+# Import our ML code
 sys.path.append("../ml")
-
 from selfAttention import SelfAttention
 from attention_utils import sent_embedd, sent_pred
 
@@ -22,6 +25,9 @@ CORS(app)
 
 
 def load_pytorch_model(modelPath):
+    '''
+    Function to load pytorch model.
+    '''
     model = SelfAttention(batch_size, output_size, hidden_size, vocab_size, 
                     embedding_length, weights)
     checkpoint = torch.load(modelPath)
@@ -29,6 +35,9 @@ def load_pytorch_model(modelPath):
     return model
 
 def load_vocab_dict(dictPath):
+    '''
+    Function to load vocab dict.
+    '''
     with open(dictPath, 'rb') as pickleDict:
     	vocab_dict = pickle.load(pickleDict)
     return vocab_dict
@@ -45,8 +54,10 @@ batch_size = 32
 max_length = 40
 weights = torch.zeros((vocab_size, embedding_length))
 
+# Use GPU if available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+# load pytorch model
 model = load_pytorch_model("../ml/models/attn_test.pth")
 model.to(device)
 
@@ -66,9 +77,6 @@ def hello():
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
 
 @app.route('/heatmap', methods = ['GET'])
 def get_classify_and_attn():
@@ -83,17 +91,23 @@ def get_classify_and_attn():
   Returns:
 	classification and attention weights
   '''
+  
+  # Get text input from request
   text = request.args.get('text', type = str)
-  
+  # tokenize text
   tokens = tokenizer(text) 
-  
+  # Get model outputs and attention matrix
   output, attention = sent_pred(text, model, vocab_dict, 
                                 tokenizer, max_length, device, batch_size)
-  
+  # Sum over attention vectorto get single value for each token
   token_attentions = attention[0].sum(axis=0).cpu().detach().tolist()[:len(tokens)]
-  
+  # Get model's class prediction
   preds = output.argmax(axis=1)
   pred = preds[0].item()
   
-  
+  # Return json response and 201 HTTP code
   return json.dumps({'class' : pred, 'tokens' : tokens, 'scores' : token_attentions}), 201
+  
+  
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
