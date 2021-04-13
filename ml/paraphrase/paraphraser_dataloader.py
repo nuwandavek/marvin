5f3910869537aff9b82f82b23188b9088e9340ac
nuwandavek <vivekaithal44@gmail.com>
2021-04-13 12:50:15 -0700

    notebook for faster prediction

--- ml/paraphrase/paraphraser_dataloader.py
+++ ml/paraphrase/paraphraser_dataloader.py
@@ -3,6 +3,7 @@ import torch
 from torch.utils.data import TensorDataset
 from tqdm import tqdm
 import numpy as np
+import pandas as pd
 
 def load_dataset(data_dir, tokenizer, mode, delimiter = "<|endoftext|>", prefix = "paraphrase: ", n_proc = 16):
     all_input_ids = None
@@ -30,4 +31,24 @@ def load_dataset(data_dir, tokenizer, mode, delimiter = "<|endoftext|>", prefix 
     return dataset
 
         
+def load_dataset_style(data_dir, tokenizer, mode, task, delimiter = "<|endoftext|>", prefix = "paraphrase: ", n_proc = 16):
+    all_input_ids = None
+    all_attention_mask = None
+    config_file = os.path.join(data_dir, task, "config.json")
+    config = json.load(open(config_file))
+    filename = os.path.join(data_dir, task, config['input_files'][mode])
+    data = pd.read_csv(filename, header=None)
+    chunk_size = len(data)//n_proc + 1
+    for chunk in tqdm(np.array_split(data, chunk_size)):
+        tokenized = tokenizer([prefix+x for x in list(chunk[0])], padding='max_length', return_tensors='pt', truncation = True)
+        if all_input_ids is None:
+            all_input_ids, all_attention_mask = tokenized['input_ids'], tokenized['attention_mask']
+        else:
+            all_input_ids = torch.cat((all_input_ids, tokenized['input_ids']),0)
+            all_attention_mask = torch.cat((all_attention_mask, tokenized['attention_mask']), 0)
+    dataset = TensorDataset(all_input_ids, all_attention_mask)
+    return dataset
+
+        
+
 
